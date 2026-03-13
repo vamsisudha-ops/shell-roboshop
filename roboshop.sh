80667a64d5ece5fa36c49f69a0898aefd4130885
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-13 19:01:30 +0530

    shell-roboshop

---
+++ roboshop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+AMI_ID=""
+SG-ID=""
+
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
+    # get private Ip 
+    if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+    else 
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+    fi 
+
+    echo "$instance: $IP"
+done 
