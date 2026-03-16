f6b54f824401125aa87249cb017192502668d8e1
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-16 12:13:11 +0530

    redis

---
+++ redis.sh
@@ -0,0 +1,46 @@
+#!/bin/bash 
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then 
+    echo "ERROR:: please run this script with root previlege"
+    exit 1 #failure is other than 0 
+fi 
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log  --> giving full path here 
+
+mkdir -p $LOGS_FOLDER   # -p checks already directory is there or not, if it's not automatically it create a directory
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE  #TO APPEND THE ECHO STATEMENT IN LOGS FILE
+
+if [ $USERID -ne 0 ]; then 
+    echo "ERROR:: please run this script with root previlege"
+    exit 1 #failure is other than 0 
+fi 
+
+VALIDATE(){ # funtions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi 
+}
+
+dnf module disable redis -y &>>$LOG_FILE
+VALIDATE $? "Disabling Default Redis"
+dnf module enable redis:7 -y &>>$LOG_FILE
+VALIDATE $? "Enabling Redis 7"
+dnf install redis -y &>>$LOG_FILE
+VALIDATE $? "Installing Redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf  # -e two change two things at a time.
+VALIDATE $? "Allowing Remote connections to Redis"
+
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling Redis"
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "Starting Redis"
